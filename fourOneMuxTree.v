7457162afdf26358ccbac4c81d5888f1fbe67b0f
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-06 08:20:09 -0500

    implemented mux tree logic

---
+++ fourOneMuxTree.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+module fourOneMuxTree(
+    input I0,
+    input I1,
+    input I2,
+    input I3,
+    input S0,
+    input S1,
+    output Y
+    );
+    
+    wire i1,i2;
+    
+    assign mux1 = twoOneMultiplexer(I0,I1,S0,1'b1,i1);
+    assign mux2 = twoOneMultiplexer(I2,I3,S0,1'b1,i2);
+    assign mux3 = twoOneMultiplexer(i1,i2,S1,1'b1,Y);
+endmodule
